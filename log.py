9119825e6973f30789daeb1bb63bc0b4a53d49fc
Amalle <273055358@qq.com>
2018-10-24 11:13:31 +0800

    add log

--- log.py
+++ log.py
@@ -1,5 +1,12 @@
 import os
+import time
+
+def logIni(file_path):
+    with open(file_path,'w') as f:
+        f.write('\n')
 
 def writeLog(file_path, str):
     with open(file_path,'a') as f:
-        f.writelines(str)
+        f.write(time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime()))
+        f.write(str)
+        f.write('\n')
